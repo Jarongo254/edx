26c372b2ec21541b77de1774b3ceaa463efa87b7
Sidney Omenya <123540913+SidneyOmenya@users.noreply.github.com>
2024-04-02 20:46:13 +0300

    Changes to be committed: 	modified:   filing.py 	modified:   names.txt 	new file:   students.csv 	new file:   students.py

--- filing.py
+++ filing.py
@@ -1,4 +1,4 @@
-name = input("what's your name ")
+#name = input("what's your name ")
 #use the open command to open a file
 # file = open("names.txt", "a") 
 # 'w' writes contents onto the file. rerunning the code overwrites the original content
@@ -7,5 +7,14 @@ name = input("what's your name ")
 # file.write(f"{name}\n")
 # file.close
 #using the 'with' argument ensures the file is open and then closed after code execution (automation)
-with open("names.txt", "a") as file:
-    file.write(f"{name}\n")
+with open("names.txt", "r") as file:
+    for line in file:
+        print("hello,", line.rstrip())
+#    file.write(f"{name}\n")
+
+#with open("names.txt", "r") as file:
+#    lines = file.readlines()
+
+#for line in lines:
+#    print("hello,", line.rstrip())
+# csv (comma separated values) - format for capturing related data e.g person-house
